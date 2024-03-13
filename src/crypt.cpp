f706ea834f71d2e1fed6507c75af0c812c4639e2
DangSage <3thandang@gmail.com>
2024-03-13 19:27:43 -0400

    add source file related -> cryptographic functions

---
+++ src/crypt.cpp
@@ -0,0 +1,13 @@
+// Cryptographic methods and functions
+// OpenSSL will be used for the cryptographic methods
+#include "_utils.hpp"
+#include "TLScope.hpp"
+#include "user.hpp"
+
+#include <openssl/evp.h>
+#include <iostream>
+#include <string>
+
+using namespace TLSS_U;
+
+

296403de27db9a852590bf0aa54e44bf71084187
DangSage <3thandang@gmail.com>
2024-03-13 11:02:11 -0400

    constants added

---
+++ include/_constants.hpp
@@ -0,0 +1,27 @@
+#ifndef _TLSS_CONSTANTS_HPP_4204
+#define _TLSS_CONSTANTS_HPP_4204
+
+#include <string>
+#include <map>
+
+namespace TLSS_C {
+
+const std::string_view VERSION = "0.0.1";
+const std::string_view AUTHOR = "Ethan Dang";
+const std::string_view TITLE_ART = R"(
+ ________  __         ______                                          
+/        |/  |       /      \                                         
+$$$$$$$$/ $$ |      /$$$$$$  |  _______   ______    ______    ______  
+   $$ |   $$ |      $$ \__$$/  /       | /      \  /      \  /      \ 
+   $$ |   $$ |      $$      \ /$$$$$$$/ /$$$$$$  |/$$$$$$  |/$$$$$$  |
+   $$ |   $$ |       $$$$$$  |$$ |      $$ |  $$ |$$ |  $$ |$$    $$ |
+   $$ |   $$ |_____ /  \__$$ |$$ \_____ $$ \__$$ |$$ |__$$ |$$$$$$$$/ 
+   $$ |   $$       |$$    $$/ $$       |$$    $$/ $$    $$/ $$       |
+   $$/    $$$$$$$$/  $$$$$$/   $$$$$$$/  $$$$$$/  $$$$$$$/   $$$$$$$/ 
+                                                  $$ |                
+                                                  $$ |                
+                                                  $$/                 )";
+
+}
+
+#endif // _TLSS_CONSTANTS_HPP_4204

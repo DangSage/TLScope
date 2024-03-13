03a1411b40b3be598b18fc80951e2582606b446e
DangSage <3thandang@gmail.com>
2024-03-13 09:18:58 -0400

    basic class implementation

--- include/TLScope.hpp
+++ include/TLScope.hpp
@@ -1,11 +1,29 @@
 // Purpose: Header file for the TLScope class.
-
-#ifndef _TS_HEADER_04022004
-#define _TS_HEADER_04022004
+#ifndef _TS_HEADER_4204
+#define _TS_HEADER_4204
 
 #include <iostream>
 #include <string>
+#include <memory>
+#include <map>
 
+// forward declarations
+class USER;
 
+// TLScope class
+class TLScope {
+public:
+    TLScope();
+    TLScope(const TLScope &rhs);
+    TLScope(const std::string &name);
+    ~TLScope();
+    TLScope &operator=(const TLScope &rhs);
+
+    void run();
+private:
+    std::shared_ptr<USER> user;
+    std::map<std::string, std::shared_ptr<USER>> registered_users;
+    std::map<std::string, std::shared_ptr<USER>> online_users;
+};
 
 #endif // _TS_HEADER_04022004

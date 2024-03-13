8196432b14dec5dbcc2ea54571259aed09a10770
DangSage <3thandang@gmail.com>
2024-03-13 09:06:16 -0400

    new header

---
+++ include/TLScope.hpp
@@ -0,0 +1,11 @@
+// Purpose: Header file for the TLScope class.
+
+#ifndef _TS_HEADER_04022004
+#define _TS_HEADER_04022004
+
+#include <iostream>
+#include <string>
+
+
+
+#endif // _TS_HEADER_04022004

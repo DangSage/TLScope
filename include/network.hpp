5509d7bda880dc5a710a828c840094a05e434f45
DangSage <3thandang@gmail.com>
2024-03-13 10:13:20 -0400

    network header

---
+++ include/network.hpp
@@ -0,0 +1,17 @@
+// network header: network class and methods
+
+#ifndef _TLSS_NETWORK_HPP_4204
+#define _TLSS_NETWORK_HPP_4204
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include <map>
+
+// forward declarations
+class USER;
+
+// network class
+class NETWORK {};
+
+#endif // _TLSS_NETWORK_HPP_4204

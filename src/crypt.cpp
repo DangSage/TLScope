93b90d8cb9e30cb6d56fae9f5eaba3c2859ee7b3
DangSage <3thandang@gmail.com>
2024-03-13 22:10:58 -0400

    genKeyPair added

--- src/crypt.cpp
+++ src/crypt.cpp
@@ -7,22 +7,28 @@
 #include <cryptopp/pwdbased.h>
 #include <cryptopp/hex.h>
 #include <cryptopp/sha.h>
+#include <cryptopp/rsa.h>
+#include <cryptopp/aes.h>
+#include <cryptopp/base64.h>
+#include <cryptopp/files.h>
 #include <cryptopp/osrng.h>
 #include <string>
 #include <sstream>
 #include <iomanip>
 
-using CryptoPP::byte;
+using namespace CryptoPP;
+
+// region Hashing and Salting
 
 std::string _rand::genSalt(size_t length) {
-    CryptoPP::AutoSeededRandomPool rng;
-    CryptoPP::SecByteBlock salt(length);
+    AutoSeededRandomPool rng;
+    SecByteBlock salt(length);
 
     rng.GenerateBlock(salt, salt.size());
 
     // Convert the binary salt into a hexadecimal string
     std::string hex_salt;
-    CryptoPP::HexEncoder encoder(new CryptoPP::StringSink(hex_salt));
+    HexEncoder encoder(new StringSink(hex_salt));
     encoder.Put(salt, salt.size());
     encoder.MessageEnd();
 
@@ -34,13 +40,13 @@ std::pair<std::string, std::string> TLSS_U::hash(const std::string& data) {
     std::string salt = _rand::genSalt(16);
 
     // hash the data with the salt
-    byte key[CryptoPP::SHA256::DIGESTSIZE];
-    CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA256> pbkdf;
+    byte key[SHA256::DIGESTSIZE];
+    PKCS5_PBKDF2_HMAC<SHA256> pbkdf;
     pbkdf.DeriveKey(key, sizeof(key), 0, (byte*)data.data(), data.size(), (byte*)salt.data(), salt.size(), 10000);
 
     // convert the key to a hexadecimal string
     std::string hex_key;
-    CryptoPP::HexEncoder encoder(new CryptoPP::StringSink(hex_key));
+    HexEncoder encoder(new StringSink(hex_key));
     encoder.Put(key, sizeof(key));
     encoder.MessageEnd();
 
@@ -48,21 +54,48 @@ std::pair<std::string, std::string> TLSS_U::hash(const std::string& data) {
 }
 
 bool TLSS_U::checkHash(const std::string& data, const std::string& salt, const std::string& hashed) {
-    byte key[CryptoPP::SHA256::DIGESTSIZE];
-    CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA256> pbkdf;
+    byte key[SHA256::DIGESTSIZE];
+    PKCS5_PBKDF2_HMAC<SHA256> pbkdf;
     pbkdf.DeriveKey(key, sizeof(key), 0, (byte*)data.data(), data.size(), (byte*)salt.data(), salt.size(), 10000);
 
     // convert the key to a hexadecimal string
     std::string hex_key;
-    CryptoPP::HexEncoder encoder(new CryptoPP::StringSink(hex_key));
+    HexEncoder encoder(new StringSink(hex_key));
     encoder.Put(key, sizeof(key));
     encoder.MessageEnd();
 
     return (hex_key == hashed);
 }
+// endregion
+
+
+// region Key Generation and Encryption
 
 std::pair<std::string, std::string> TLSS_U::genKeyPair() {
-    return std::make_pair("public_key", "private");
+    AutoSeededRandomPool rng;
+    
+    // Generate private key
+    RSA::PrivateKey privateKey;
+    privateKey.GenerateRandomWithKeySize(rng, 2048);
+
+    // Generate public key
+    RSA::PublicKey publicKey(privateKey);
+
+    // Convert the keys to strings
+    std::string private_key;
+    std::string public_key;
+
+    // Convert the private and public keys to strings
+    // * Sink objects convert keys (bytes) -> strings (base64)
+    Base64Encoder privateSink(new StringSink(private_key));
+    privateKey.DEREncode(privateSink);
+    privateSink.MessageEnd();
+
+    Base64Encoder publicSink(new StringSink(public_key));
+    publicKey.DEREncode(publicSink);
+    publicSink.MessageEnd();
+
+    return std::make_pair(private_key, public_key);
 }
 
 // encrypted_file_info = {

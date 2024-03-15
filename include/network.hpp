dad7f9e513ac8b390bb3f4657ff59f7a682bb91b
DangSage <3thandang@gmail.com>
2024-03-15 01:14:09 -0400

    redefined

--- include/network.hpp
+++ include/network.hpp
@@ -16,9 +16,41 @@ class USER;
 // network class
 // * will contain all network related classes and methods
 // * needs to be able to send and receive data
-class NETWORK {
+class NETWORK_MANAGER {
 public:
-    NETWORK() = default;
+    NETWORK_MANAGER() {
+    //  session_token()
+    // gen_certificate(gl.USER_EMAIL)
+    // write_session_token()
+    // ng.bcast_port, ng.tcp_listen = port_manager(ng.bcast_port, ng.tcp_listen)
+
+    // # broadcast socket setup (multicast)
+    // broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    // broadcast_socket.bind(("", ng.bcast_port))
+    
+
+    // udp_listener = Thread(
+    //     target=broadcast_listen,
+    //     name="broadcast_listener",
+    //     args=(broadcast_socket,)
+    // )
+
+    // udp_sender = Thread(
+    //     target=broadcast_send,
+    //     name="broadcast_sender",
+    //     args=(ng.bcast_port,)
+    // )
+
+    // tcp_listener = Thread(
+    //     target=tcp_listen,
+    //     name="tcp_listener",
+    //     args=(ng.tcp_listen,)
+    // )
+    }
+
+    ~NETWORK_MANAGER () {
+        // broadcast_socket.close()
+    }
 
 private:
     // my ip address

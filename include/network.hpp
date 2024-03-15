78759b20f9931a4fd7fc0de3d79c84b10f0628d0
DangSage <3thandang@gmail.com>
2024-03-15 01:11:06 -0400

    network class defined

--- include/network.hpp
+++ include/network.hpp
@@ -6,12 +6,35 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <openssl/ssl.h>
+#include <vector>
 #include <map>
 
 // forward declarations
 class USER;
 
 // network class
-class NETWORK {};
+// * will contain all network related classes and methods
+// * needs to be able to send and receive data
+class NETWORK {
+public:
+    NETWORK() = default;
+
+private:
+    // my ip address
+    std::string ip;
+    // session token
+    std::string token;
+    // list of online users on the network
+    std::map<std::string, USER> onlineUsers;
+    // list of trusted users on the network
+    std::map<std::string, USER> trustedUsers;
+    // list of contact requests received by the client
+    std::vector<std::string> contactRequestsReceived;
+    // list of pending contact requests sent by the client
+    std::vector<std::string> pendingContactRequests;
+    // list of ports to ignore when broadcasting
+    std::vector<int> portsToIgnore;
+};
 
 #endif // _TLSS_NETWORK_HPP_4204

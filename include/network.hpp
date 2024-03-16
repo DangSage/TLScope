8243399cae8386ef6459750973b26acec4a93665
DangSage <3thandang@gmail.com>
2024-03-15 23:23:13 -0400

    conceptualized netmanager

--- include/network.hpp
+++ include/network.hpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <memory>
 #include <openssl/ssl.h>
+#include <mutex>
 #include <vector>
 #include <map>
 
@@ -16,13 +17,29 @@ class USER;
 // network class
 // * will contain all network related classes and methods
 // * needs to be able to send and receive data
+// ** UDP connections for device discovery (ping w/ DTLS)
+// ** TCP connections for sending and receiving messages (TLS)
 class NetManager {
 public:
     // initialize openssl
     // create a new SSL context
     // create socket and open connection
+    // run the threads for the client and server connections
     NetManager();
     ~NetManager();
+
+
+    // UDP client thread to ping devices listening on the designated port
+    void UDPClient(const std::string& ip);
+    
+    // UDP server thread to listen for pings on the designated port
+    void UDPServer(const std::string& ip);
+
+    // TCP client thread to send messages
+    void TCPClient(const std::string& ip, int port);
+
+    // TCP server thread to listen for messages 
+    void TCPServer(const std::string& ip, int port);
 
 
 private:
@@ -32,6 +49,7 @@ private:
     std::string ip;
     std::string token;
     std::map<std::string, std::shared_ptr<USER>> onlineUsers;
+    std::mutex onlineUsersMutex;
 
     void initOpenSSL();
     void cleanupOpenSSL();

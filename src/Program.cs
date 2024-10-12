1a6513317b04829767e63459543a924a70ccddaa
Ethan Dang <3thandang@gmail.com>
2024-10-12 14:49:16 -0400

    example pinger list

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,9 @@
 ﻿using TLScope.src.Models;
 using TLScope.src.Services;
+using Terminal.Gui;
 using System;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace TLScope
 {
@@ -15,11 +17,81 @@ namespace TLScope
 
         static async Task MainAsync(string[] args)
         {
-            // Create a new instance of the NetworkService class
-            NetworkService networkService = new();
+            try
+            {
+                // Create a new instance of the NetworkService class
+                NetworkService networkService = new();
 
-            // Call the DiscoverLocalNetworkAsync method to discover devices on the local network
-            await networkService.DiscoverLocalNetworkAsync();
+                // ## GUI & Visualization
+                // - Terminal.Gui (1.4.0+): Text-based UI for terminal applications
+
+                Application.Init();
+
+                var colorS = new ColorScheme {
+                    Normal = Terminal.Gui.Attribute.Make(Color.White, Color.Black),
+                    Focus = Terminal.Gui.Attribute.Make(Color.Black, Color.White),
+                    HotNormal = Terminal.Gui.Attribute.Make(Color.White, Color.Black),
+                    HotFocus = Terminal.Gui.Attribute.Make(Color.Black, Color.White),
+                };
+
+                var win = new Window("TLScope - CTRL-C to quit")
+                {
+                    X = 2,
+                    Y = 2,
+                    Width = Dim.Fill(2),
+                    Height = Dim.Fill(2),
+                    ColorScheme = colorS
+                };
+
+                // Create a Label to display network information
+                var networkInfoLabel = new Label("Discovering network devices...")
+                {
+                    X = 1,
+                    Y = 1,
+                    Width = Dim.Fill(),
+                    Height = 1
+                };
+                win.Add(networkInfoLabel);
+
+                Application.Top.Add(win);
+
+                // Add a key binding for Ctrl+Q to exit the application
+                Application.Top.Add(new MenuBar(new MenuBarItem[] {
+                    new MenuBarItem("_File", new MenuItem[] {
+                        new MenuItem("_Quit", "", () => Application.RequestStop(), null, null, Key.CtrlMask | Key.C)
+                    })
+                }));
+
+                // Start a background task for network discovery
+                _ = Task.Run(async () =>
+                {
+                    await networkService.DiscoverLocalNetworkAsync();
+                });
+
+                // Use a Timer to periodically update the networkInfoLabel
+                var timer = new Timer((state) =>
+                {
+                    Application.MainLoop.Invoke(() =>
+                    {
+                        networkInfoLabel.Text = "Discovered devices:\n";
+                        foreach (var device in networkService.activeDevices)
+                        {
+                            networkInfoLabel.Text += $"{device.Key}: {(device.Value ? "Active" : "Inactive")}\n";
+                        }
+                    });
+                }, null, 0, 1000); // Update every second
+
+                Application.Run();
+                // Dispose the timer when the application shuts down
+                timer.Dispose();
+                Application.Shutdown();
+
+                await Task.WhenAll();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
     }
 }

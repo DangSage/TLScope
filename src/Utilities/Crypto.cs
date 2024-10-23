fa8cd22339ba56917d4d307d173846e9a9566327
Ethan Dang <3thandang@gmail.com>
2024-10-22 22:30:04 -0400

    more consistent casing

--- src/Utilities/Crypto.cs
+++ src/Utilities/Crypto.cs
@@ -13,15 +13,13 @@ namespace TLScope.src.Utilities
         /// <param name="password">The password to hash.</param>
         /// <param name="passwordHash">The resulting password hash.</param>
         /// <param name="passwordSalt">The salt used in the hashing process.</param>
-        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-        {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) {
             // Generate a random salt
             passwordSalt = new byte[16];
             RandomNumberGenerator.Fill(passwordSalt);
 
             // Use Argon2id for password hashing
-            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
-            {
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password)) {
                 // Set the salt for the hashing process
                 Salt = passwordSalt,
                 // Set the degree of parallelism (number of threads to use)
@@ -43,11 +41,9 @@ namespace TLScope.src.Utilities
         /// <param name="storedHash">The stored password hash.</param>
         /// <param name="storedSalt">The stored salt used in the hashing process.</param>
         /// <returns>True if the password is correct, otherwise false.</returns>
-        public static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
-        {
+        public static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt) {
             // Use Argon2id for password hashing
-            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
-            {
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password)) {
                 // Set the salt for the hashing process
                 Salt = storedSalt,
                 // Set the degree of parallelism (number of threads to use)
@@ -62,10 +58,8 @@ namespace TLScope.src.Utilities
             var computedHash = argon2.GetBytes(32);
 
             // Compare the computed hash with the stored hash
-            for (int i = 0; i < storedHash.Length; i++)
-            {
-                if (computedHash[i] != storedHash[i])
-                {
+            for (int i = 0; i < storedHash.Length; i++) {
+                if (computedHash[i] != storedHash[i]) {
                     return false;
                 }
             }

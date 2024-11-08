4f303d710c6356ecbeafa6a102154c3fa230962b
Ethan Dang <3thandang@gmail.com>
2024-11-08 10:45:55 -0500

    docu comment

--- src/Models/AssemblyInfo.cs
+++ src/Models/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+// Make sure the NetworkController class is public and the GetActiveDevices method is public as well.
+
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("TLScope.src.Services")]
